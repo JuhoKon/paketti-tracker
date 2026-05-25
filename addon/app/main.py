dff2c3bcacd442b9eed4019fb416c8fab978fb35
Juho Kontiainen <juho.kontiainen@twoday.com>
2026-05-25 11:09:05 +0300

    chore: update version

--- addon/app/main.py
+++ addon/app/main.py
@@ -115,7 +115,7 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
 
 app = FastAPI(
     title="Paketti Tracker",
-    version="0.5.0",
+    version="0.6.0",
     lifespan=lifespan,
 )
 
